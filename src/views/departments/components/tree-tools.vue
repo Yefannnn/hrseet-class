0bae6c80cae691dac006872cbcd7ca1046181cc5
Yefan <Yefan97114@qq.com>
2022-07-03 18:37:57 +0800

    随手备份

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,47 @@
+<template>
+  <!-- 行 -->
+  <el-row type="flex" justify="space-bewteen" align="middle" style="height: 40px ; width:100%">
+    <!-- 列 -->
+    <el-col>
+      <span>{{ treeNode.name }}</span>
+    </el-col>
+    <el-col :span="4" align>
+      <el-row type="flex" justify="end">
+        <el-col>{{ treeNode.manager }}</el-col>
+        <el-col>
+          <el-dropdown trigger="click">
+            <span>
+              操作<i class="el-icon-arrow-down" />
+            </span>
+            <!-- 下拉菜单 -->
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">编辑子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">删除子部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'TreeTools',
+  props: {
+    treeNode: {
+      required: true,
+      type: Object
+    },
+    isRoot: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -2,39 +2,39 @@
   <div class="dashboard-container">
     <div class="app-container">
       <el-card>
-        <!-- 行 -->
-        <el-row type="flex" justify="space-bewteen" align="middle" style="height: 40px">
-          <!-- 列 -->
-          <el-col>
-            <h3>江苏省南京市黑马程序员</h3>
-          </el-col>
-          <el-col :span="4" align>
-            <el-row type="flex" justify="end">
-              <el-col>负责人</el-col>
-              <el-col>
-                <el-dropdown>
-                  <span>
-                    操作<i class="el-icon-arrow-down" />
-                  </span>
-                  <!-- 下拉菜单 -->
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-
-            </el-row>
-
-          </el-col>
-        </el-row>
+        <TreeTools :tree-node="company" :is-root="true" />
+        <!-- 树形结构 -->
+        <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
+          <template v-slot="{ data }">
+            <TreeTools :tree-node="data" />
+          </template>
+        </el-tree>
       </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import TreeTools from './components/tree-tools.vue'
 export default {
-  name: 'Departments'
+  name: 'Departments',
+  components: {
+    TreeTools
+  },
+  data() {
+    return {
+      company: { name: '南京黑马程序员', manager: '负责人' },
+      departs: [{ name: 'yefan', manager: 'yefan',
+        deps: [{
+          name: '1111', manager: '1111'
+        }]
+      }],
+      defaultProps: {
+        label: 'name',
+        children: 'deps'
+      }
+    }
+  }
 }
 </script>
 
